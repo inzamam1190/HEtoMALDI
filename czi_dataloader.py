b6cb0bd69c7795cdafb6d00c7aaff1ad1c353d48
Md Inzamam Ul Haque (64f) <64f@lucky.ornl.gov>
2021-11-11 23:24:18 -0500

    Add transforms

--- czi_dataloader.py
+++ czi_dataloader.py
@@ -15,18 +15,16 @@ class CZIPatchDataset(Dataset):
     def __init__(self, img_dir, transform=None):
         self.img_dir = img_dir 
         self.transform = transform
+        self.filenames = sorted([name for name in os.listdir(self.img_dir) if os.path.isfile(os.path.join(self.img_dir, name))])
 
     def __len__(self):
-        return len([name for name in os.listdir(self.img_dir) if os.path.isfile(os.path.join(self.img_dir, name))])
+        return len(self.filenames)
 
     def __getitem__(self, idx):
-        img_path = os.listdir(self.img_dir)[idx]
-        dict_patch = torch.load(os.path.join(self.img_dir,img_path))
+        #img_path = os.listdir(self.img_dir)[idx]
+        dict_patch = torch.load(os.path.join(self.img_dir,self.filenames[idx]))
         # get patch
         image = dict_patch.get('data') 
-        # normalize to range 0-1
-        image -= image.min(1, keepdim=True)[0]
-        image /= image.max(1, keepdim=True)[0]
         # change dimension from HWC to CHW 
         image = torch.permute(image, (2, 0, 1))
         # get label
@@ -39,23 +37,26 @@ class CZIDataModule(pl.LightningDataModule):
     def __init__(self, data_dir):
         super().__init__()
         self.data_dir = data_dir
-        self.transform = None
-        #self.dims = (3, 512, 512)
+        self.transform = transforms.Compose([transforms.Normalize(mean=[696.5508, 479.1579, 525.0390],
+                                              std=[247.2921, 204.0386, 174.9929]),
+                                             transforms.ToPILImage(),
+                                             transforms.ToTensor()])
 
     def setup(self):
 
-        data_full = CZIPatchDataset(self.data_dir) #get the full dataset
-        self.train, self.val, self.test = random_split(data_full, [6605, 1000, 2000], generator=torch.Generator().manual_seed(42))
+        # Use train_test_split from filenames and split the data
+        data_full = CZIPatchDataset(img_dir=self.data_dir, transform=self.transform) #get the full dataset
+        self.train, self.val, self.test = random_split(data_full, [5000, 1000, 1225], generator=torch.Generator().manual_seed(42))
 
 
     def train_dataloader(self):
-        return DataLoader(self.train, batch_size=32)
+        return DataLoader(self.train, batch_size=32, shuffle=True, pin_memory=True, num_workers=10)
 
     def val_dataloader(self):
-        return DataLoader(self.val, batch_size=32)
+        return DataLoader(self.val, batch_size=32, shuffle=True, pin_memory=True, num_workers=10)
 
     def test_dataloader(self):
-        return DataLoader(self.test, batch_size=32)
+        return DataLoader(self.test, batch_size=32, shuffle=True, pin_memory=True, num_workers=10)
 
 if __name__ == '__main__':
     import argparse
@@ -67,7 +68,7 @@ if __name__ == '__main__':
     #dataset = CZIPatchDataset(dir)
     dm = CZIDataModule(args.datapath)
     dm.setup()
-    #dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+    
     train_features, train_labels = next(iter(dm.train_dataloader()))
     print(f"Feature batch shape: {train_features.size()}")
     print(f"Labels batch shape: {train_labels.size()}")
