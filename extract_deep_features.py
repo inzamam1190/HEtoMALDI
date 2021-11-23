5bb0d15e12a874bcdcc4ebf6efc311a18f41ccfd
Md Inzamam Ul Haque (64f) <64f@lucky.ornl.gov>
2021-11-23 10:36:49 -0500

    Add halo and remove permutation

--- extract_deep_features.py
+++ extract_deep_features.py
@@ -9,66 +9,59 @@ from tqdm import tqdm
 from torch import nn
 from torchinfo import summary
 from torchvision import models
+from torchvision import transforms
 
 # Need to read more on hooks
 def get_features(name):
     def hook(model, input, output):
         features[name] = output.detach()
-
     return hook
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     import argparse
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--datapath', '-d', nargs=2, required='True',
+            help='Hdf5 File to extract features from followed by the dataset name')
+    parser.add_argument('--target_file', '-t', nargs=2, required='True',
+            help="HDF5 filename followed by dataset name to write. Must not exist.")
+    parser.add_argument(
+        "--patch_size", 
+        default=2048,
+        type=int,
+        help="Size of square patches in pixels (not counting halos).")
+    parser.add_argument(
+        "--halo_width", 
+        default=0,
+        type=int,
+        help="Size of halos to add to patch size on each side.")
 
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    parser.add_argument(
-        "--datapath",
-        "-d",
-        nargs=2,
-        required="True",
-        help="Hdf5 File to extract features from followed by the dataset name",
-    )
-    parser.add_argument(
-        "--target_file",
-        "-t",
-        nargs=2,
-        required="True",
-        help="HDF5 filename followed by dataset name to write. Must not exist.",
-    )
-    parser.add_argument(
-        "--patch_size",
-        "-p",
-        required="True",
-        help="patchsize from input image for passing to the model to extract features",
-    )
 
     args = parser.parse_args()
 
     # define model
     model = models.segmentation.fcn_resnet101(pretrained=True)
-    # print model
-    print(summary(model))
+    #print model
+    summary(model)
 
     file, dataset = args.datapath
     target_file, target_dataset = args.target_file
-    M = int(args.patch_size)
-
+    M = args.patch_size
+    H = args.halo_width
+    tx = transforms.Normalize(mean=[696.5508, 479.1579, 525.0390], std=[247.2921, 204.0386, 174.9929])
+    
     # register hook to a layer. can always change the layer. This line will change depending one the model
-    model.backbone.layer2[3].conv3.register_forward_hook(
-        get_features("feats")
-    )  # shape(c,h,w)-->(c,64,64)
+    model.backbone.layer4.register_forward_hook(get_features('feats')) #shape(c,h,w)-->(c,64,64)
     device = torch.device("cuda:0")
     model = model.to(device)
 
     # test if hook works
-    features = {}
-    random_input = torch.zeros((1, 3, 512, 512), dtype=torch.float32, device="cuda:0")
-    out = model(random_input)["out"]
-    fts = features["feats"].squeeze().cpu().numpy()
-    print(fts.shape)  # should be (M,64,64)
+    features={}
+    random_input = torch.zeros((1,3,M,M), dtype=torch.float32, device='cuda:0')
+    out = model(random_input)['out']
+    fts = features['feats'].squeeze().cpu().numpy()
+    print(fts.shape) 
+    #downsample value
+    div = M//fts.shape[1]
     del features
 
     with h5py.File(file, "r") as fsource:
@@ -79,29 +72,39 @@ if __name__ == "__main__":
                     f"Refusing to overwriting existing dataset {target_dataset} in {target_file}"
                 )
             ds_target = ftarget.create_dataset(
-                target_dataset,
-                shape=(
-                    M,
-                    ds_source.shape[0] // (M / fts.shape[1]),
-                    ds_source.shape[1] // (M / fts.shape[2]),
-                ),
-                chunks=True,
-                dtype="f",
-            )
+                target_dataset, shape=(fts.shape[0],ds_source.shape[1]//div,ds_source.shape[2]//div), chunks=True, dtype='f'
+                )
 
-            for i in tqdm(range(0, ds_source.shape[0], M)):
-                for j in range(0, ds_source.shape[1], M):
-                    # Load patch
-                    features = {}
-                    endy = min(i + M, ds_source.shape[0])
-                    endx = min(j + M, ds_source.shape[1])
-                    patch = torch.tensor(ds_source[i:endy, j:endx, :]).cuda()
-                    patch = torch.permute(patch, (2, 0, 1)).unsqueeze(0)
-                    out = model(patch)["out"]
-                    ft = features["feats"].squeeze().cpu().numpy()
-                    ds_target[
-                        :,
-                        i : endy // (M // ft.shape[1]),
-                        j : endx // (M // ft.shape[2]),
-                    ] = ft
-                    del features
+            with torch.no_grad():
+                for i in tqdm(range(0, ds_source.shape[-1]-M+1, M)):
+                    for j in range(0, ds_source.shape[-2]-M+1, M):
+                        
+                        features = {}
+                        # Load patch with halo
+                        starty = max(i - H, 0)
+                        startx = max(j - H, 0)
+                        endy = min(i + M + H, ds_source.shape[-1])
+                        endx = min(j + M + H, ds_source.shape[-2])
+                        # compute interior bounds
+                        intstarty = i
+                        intstartx = j
+                        intendy = min(i + M, ds_source.shape[-1])
+                        intendx = min(j + M, ds_source.shape[-2])
+
+                        patch = torch.tensor(ds_source[:, startx:endx, starty:endy]).cuda()
+                        patch = patch.unsqueeze(0)
+                        #standardize
+                        patch = tx(patch)
+
+                        #get features
+                        out = model(patch)['out']
+                        ft = features['feats'].squeeze().cpu().numpy()
+                        ds_target[:, intstartx:(intendx//div), intstarty:(intendy//div)] = ft[:,((H//div)//2):(ft.shape[1]-((H//div)//2)),
+                                                                                            ((H//div)//2):(ft.shape[1]-((H//div)//2))]
+                        del features
+                        
+
+
+    
+
+
